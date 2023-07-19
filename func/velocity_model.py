aef315359616da4418f9555ce27970dd9ba2ff19
medicinely <yuke.xie@icloud.com>
2023-07-19 15:05:15 +0200

    Yuke

--- func/velocity_model.py
+++ func/velocity_model.py
@@ -7,6 +7,9 @@ from scipy.sparse import *
 import numpy as np
 import matplotlib.pyplot as plt
 
+import gstlearn as gl
+from sksparse.cholmod import cholesky
+
 
 def check_model(v, vmin=2000, vmax=3000, disp=False):
     # v[np.where(v > vmax)] = vmax
@@ -27,34 +30,33 @@ def check_model(v, vmin=2000, vmax=3000, disp=False):
         print("-- MODEL CHECKED --")
 
 
-def gaussian2d(ranges=[5, 10], variance=1, param=1, nx=[201, 201], mean=0):
-    import gstlearn as gl
-    from sksparse.cholmod import cholesky   
+def gaussian2d(ranges = [5,10], variance = 10, param = 1, nx = [100,100],mu=2500):
     #Creation of the covariance model (Whittle-Matérn prior)
-    model = gl.Model.createFromParam(gl.ECov.BESSEL_K,
-                                     param=param,
-                                     sill=variance,
-                                     ranges=ranges)
-
-    #Creation of the grid
+    model = gl.Model.createFromParam(gl.ECov.BESSEL_K,param = param,
+                                     sill = variance, ranges = ranges)
+    
+    #Creation of the grid 
     mesh = gl.MeshETurbo(nx)
-
+    
     #Creation of the precision matrix (inverse of the covariance)
-    precisionOpMat = gl.PrecisionOpCs(mesh, model, 0, gl.EPowerPT.ONE, False)
+    precisionOpMat = gl.PrecisionOpCs(mesh,model,0,gl.EPowerPT.ONE,False)
     Qtr = gl.csToTriplet(precisionOpMat.getQ())
-    Qmat = sc.sparse.csc_matrix(
-        (np.array(Qtr.values), (np.array(Qtr.rows), np.array(Qtr.cols))))
+    Qmat = sc.sparse.csc_matrix((np.array(Qtr.values), (np.array(Qtr.rows), np.array(Qtr.cols))))
+    
+    ind = np.arange(Qmat.shape[0])
+    ind =np.reshape(ind,(nx[1],nx[0])).T.flatten()
+    Qmat = Qmat[ind,:][:,ind]
 
     #Cholesky decomposition
     cholQ = cholesky(Qmat)
-
+    
     #Random vector
     u = np.random.normal(size=np.prod(Qmat.shape[0]))
-
+    
     # Apply the inverse of the Cholesky decomposition
-
+    
     simu = cholQ.apply_Pt(cholQ.solve_DLt(u))
-
+    
     #Return the simulation (reshaped)
-
-    return simu.reshape(mesh.getGrid().getNXs()) + mean, Qmat
+    
+    return simu.reshape(mesh.getGrid().getNXs())+mu, Qmat
